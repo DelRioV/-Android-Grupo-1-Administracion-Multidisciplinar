9d7ecb89dc40774f7cb22d44723de5eb1e36b23c
pablo <pablosalvadordelriovergara.sanjose@alumnado.fundacionloyola.net>
2022-12-20 19:10:08 +0100

    Second commit

--- app/src/main/java/com/example/manual/MainActivity.java
+++ app/src/main/java/com/example/manual/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.manual;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.);
+        setContentView(R.layout.mainactivitylayout);
+        Statement st = new ConnectionDB().getRemoteConnection();
+        ResultSet rs;
+        TextView tv = findViewById(R.id.textView);
+        try {
+            rs = st.executeQuery("SELECT * FROM users");
+            while (rs.next()) {
+                tv.setText(tv.getText() + rs.getString(1) + "\n");
+            }
+        } catch (SQLException e) {
+        }
 
     }
 }
