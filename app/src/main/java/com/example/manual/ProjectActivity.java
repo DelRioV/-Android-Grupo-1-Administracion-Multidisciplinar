79886d52bdbcbe78c477c2c150ba4313bb934062
pablo <pablosalvadordelriovergara.alumnado@sanjose.fundacionloyola.net>
2022-12-23 10:57:36 +0100

    Adding comments

--- app/src/main/java/com/example/manual/ProjectActivity.java
+++ app/src/main/java/com/example/manual/ProjectActivity.java
@@ -3,6 +3,7 @@ package com.example.manual;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProjectActivity extends Activity {
 
@@ -12,5 +13,7 @@ public class ProjectActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.projectlayout);
         findViewById(R.id.btnBack2).setOnClickListener(new ListenerBackMenu(this));
+        TextView tv = findViewById(R.id.tvProject);
+        tv.setText("Project made by:\nPablo Salvador Del Río Vergara\nIsmael Orellana Bello\nJorge Luis López Pérez\nÁngel Acedo Moreno\nJavier Tienda\nJose Ramón Gallego");
     }
 }
